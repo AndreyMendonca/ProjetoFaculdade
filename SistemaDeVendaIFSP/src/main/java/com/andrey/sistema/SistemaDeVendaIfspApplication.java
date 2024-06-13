973e4389c115b9fe6246e5aa63ab7e97aa28b7b3
Andrey Mendonca <andrey.mmendonca@gmail.com>
2024-06-13 18:55:09 -0300

    Projeto BackEnd Criado

---
+++ SistemaDeVendaIFSP/src/main/java/com/andrey/sistema/SistemaDeVendaIfspApplication.java
@@ -0,0 +1,13 @@
+package com.andrey.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeVendaIfspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeVendaIfspApplication.class, args);
+	}
+
+}
---
+++ SistemaDeVendaIFSP/src/test/java/com/andrey/sistema/SistemaDeVendaIfspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrey.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeVendaIfspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
